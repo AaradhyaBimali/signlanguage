37c093989df45f4baa30ee8b77aef67914eebaf9
AaradhyaBimali <aaradhyabimali999@gmail.com>
2025-12-16 20:03:05 +0545

    train model

--- code/trainmodel.py
+++ code/trainmodel.py
@@ -9,7 +9,7 @@ import numpy as np
 import os
 
 #map actions to numerical values
-label_map=(label:num for num,label in enumerate(actions))
+label_map={label:num for num,label in enumerate(actions)}
 
 #load and pad sequences
 sequences, labels=[], []
@@ -28,3 +28,41 @@ for action in actions:
 
         sequences.append(window) #append sequence of keypoints
         labels.append(label_map[action]) #append corresponding label
+
+
+
+#prepare the training and validation data
+X=np.array(sequences)
+y=to_categorical(labels).astype(int)
+X_train, X_test, y_train, y_test=train_test_split(X,y,test_size=0.05,stratify=y) #5% data for testing
+
+
+#saving the logs of tensorboard
+log_dir=os.path.join('logs')
+tb_callback=TensorBoard(log_dir=log_dir)
+
+#define a model
+model=Sequential([
+    LSTM(64, return_sequences=True, activation='relu', input_shape=(30,63)),
+    LSTM(128, return_sequences=True, activation='relu'),
+    LSTM(64, return_sequences=False, activation='relu'),
+    Dense(64, activation='relu'),
+    Dense(32, activation='relu'),
+    Dense(actions.shape[0], activation='softmax')
+])
+
+#compile the model
+model.compile(optimizer='Adam', loss='categorical_crossentropy', metrics=['categorical_accuracy'])
+
+#train the model
+model.fit(X_train, y_train, epochs=2000, callbacks=[tb_callback], validation_data=(X_test, y_test))
+model.summary()
+
+#save the model
+model_json=model.to_json()
+
+with open('model.json','w') as json_file:
+    json_file.write(model_json)
+
+model.save_weights('model.h5')
+print('Model saved successfully')
