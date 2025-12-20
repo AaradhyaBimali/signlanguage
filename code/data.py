9add9ed6d2f2159a05de95fad0d8d83e4666f6be
AaradhyaBimali <aaradhyabimali999@gmail.com>
2025-12-20 19:44:57 +0545

    edited

--- code/data.py
+++ code/data.py
@@ -13,40 +13,43 @@ with mp_hands.Hands(
     min_detection_confidence=0.3, #minimum confidence value to detect hand/threshold
     min_tracking_confidence=0.3 #minimum confidence value to track hand
 ) as hands:
-    
+    exit_flag = False
     for action in actions:
+        if exit_flag:
+            break
         for sequence in range(no_sequences):
-            for frame_num in range(sequence_length):
-                frame=cv2.imread(f'image/{action}/{sequence}.png') #read image from collected data
-            
-            if frame is None:
-                print("Image not found")
-                continue
-
-            image, results = mediapipe_detection(frame, hands) #detect hand landmarks
-
-            #check for hand landmarks in frame
-            if results.multi_hand_landmarks:
-                print(f'Hand detected for action {action}, sequence {sequence}, frame {frame_num}')
-            else:
-                print(f'No hand detected for action {action}, sequence {sequence}, frame {frame_num}')
-
-            draw_styles_landmarks(image, results) #draw hand landmarks on image
-
-            #display collection status
-            message=(f'Collecting data for action {action}, sequence {sequence}, frame {frame_num}')
-            cv2.putText(image, message, (15,12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255),1,cv2.LINE_AA) #put text on image
-            cv2.imshow('OpenCV Feed', image) #show image with landmarks
-
-            #extract and save keypoints
-            keypoints=extract_keypoints(results)
-            npy_path=os.path.join(DATA_PATH, action, str(sequence), str(frame_num))
-            np.save(npy_path, keypoints)
-
-
-            if cv2.waitKey(10) & 0xFF==ord('q'): #press q to quit
+            if exit_flag:
                 break
+            for frame_num in range(sequence_length):
+                frame=cv2.imread(f'image/{action}/{frame_num + sequence * sequence_length}.png') #read image from collected data
+            
+                if frame is None:
+                    print("Image not found")
+                    continue
+
+                image, results = mediapipe_detection(frame, hands) #detect hand landmarks
+
+                #check for hand landmarks in frame
+                if results.multi_hand_landmarks:
+                    print(f'Hand detected for action {action}, sequence {sequence}, frame {frame_num}')
+                else:
+                    print(f'No hand detected for action {action}, sequence {sequence}, frame {frame_num}')
+
+                draw_landmarks(image, results) #draw hand landmarks on image
+
+                #display collection status
+                message=(f'Collecting data for action {action}, sequence {sequence}, frame {frame_num}')
+                cv2.putText(image, message, (15,12), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255),1,cv2.LINE_AA) #put text on image
+                cv2.imshow('OpenCV Feed', image) #show image with landmarks
+
+                #extract and save keypoints
+                keypoints=extract_keypoints(results)
+                npy_path=os.path.join(DATA_PATH, action, str(sequence), str(frame_num))
+                np.save(npy_path, keypoints)
+
+
+                if cv2.waitKey(10) & 0xFF==ord('q'): #press q to quit
+                    exit_flag = True
+                    break
 
 cv2.destroyAllWindows() #close all cv2 windows
-
-            
